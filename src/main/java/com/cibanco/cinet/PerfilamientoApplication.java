3c32f96f49c979fa40ca014ad182d5afe5140d1b
davidolveralira <53874299+davidolveralira@users.noreply.github.com>
2024-12-30 12:38:15 -0600

    Initial commit

---
+++ src/main/java/com/cibanco/cinet/PerfilamientoApplication.java
@@ -0,0 +1,13 @@
+package com.cibanco.cinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfilamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfilamientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cibanco/cinet/PerfilamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibanco.cinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfilamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
